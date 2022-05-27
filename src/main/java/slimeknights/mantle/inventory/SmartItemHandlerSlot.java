0d1615cf25e19b995a01a67e91c9f446a165ae08
alpha <alphamodeyt@gmail.com>
2022-05-27 18:01:08 -0500

    make SmartItemHandlerSlot require IItemHandler instead of ItemStackHandlerForge

--- src/main/java/slimeknights/mantle/inventory/SmartItemHandlerSlot.java
+++ src/main/java/slimeknights/mantle/inventory/SmartItemHandlerSlot.java
@@ -1,18 +1,17 @@
 package slimeknights.mantle.inventory;
 
-import io.github.fabricators_of_create.porting_lib.transfer.item.ItemStackHandlerForge;
+import io.github.fabricators_of_create.porting_lib.transfer.item.IItemHandler;
 import io.github.fabricators_of_create.porting_lib.transfer.item.SlotItemHandlerForge;
 import net.minecraft.world.item.ItemStack;
 
 /** Forge still uses dumb vanilla logic for determining slot limits instead of their own method */
-@SuppressWarnings("UnstableApiUsage")
 public class SmartItemHandlerSlot extends SlotItemHandlerForge {
-	public SmartItemHandlerSlot(ItemStackHandlerForge itemHandler, int index, int xPosition, int yPosition) {
-		super(itemHandler, index, xPosition, yPosition);
-	}
+  public SmartItemHandlerSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+    super(itemHandler, index, xPosition, yPosition);
+  }
 
-	@Override
-	public int getMaxStackSize(ItemStack stack) {
-		return getItemHandler().getSlotLimit(getSlotIndex());
-	}
+  @Override
+  public int getMaxStackSize(ItemStack stack) {
+    return getItemHandler().getSlotLimit(getSlotIndex());
+  }
 }
