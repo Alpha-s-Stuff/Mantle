ed2b6c02b513e29d2595f4df3a5696dcf902cb53
alpha <alphamodeyt@gmail.com>
2022-07-14 17:50:02 -0500

    bump port lib

--- src/main/java/slimeknights/mantle/datagen/MantleFluidTagProvider.java
+++ src/main/java/slimeknights/mantle/datagen/MantleFluidTagProvider.java
@@ -1,5 +1,7 @@
 package slimeknights.mantle.datagen;
 
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.FluidTagsProvider;
 import net.minecraft.world.level.material.Fluids;
@@ -11,13 +13,13 @@ import static slimeknights.mantle.datagen.MantleTags.Fluids.LAVA;
 import static slimeknights.mantle.datagen.MantleTags.Fluids.WATER;
 
 /** Provider for tags added by mantle, generally not useful for other mods */
-public class MantleFluidTagProvider extends FluidTagsProvider {
-  public MantleFluidTagProvider(DataGenerator gen) {
+public class MantleFluidTagProvider extends FabricTagProvider.FluidTagProvider {
+  public MantleFluidTagProvider(FabricDataGenerator gen) {
     super(gen);
   }
 
   @Override
-  protected void addTags() {
+  protected void generateTags() {
     this.tag(WATER).add(Fluids.WATER, Fluids.FLOWING_WATER);
     this.tag(LAVA).add(Fluids.LAVA, Fluids.FLOWING_LAVA);
   }
