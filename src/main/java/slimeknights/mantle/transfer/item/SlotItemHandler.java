220bf5b45e8852038ae392e187fd7203b240296e
alpha <alphamodeyt@gmail.com>
2022-07-23 20:00:30 -0500

    why not have both

--- src/main/java/slimeknights/mantle/transfer/item/SlotItemHandler.java
+++ src/main/java/slimeknights/mantle/transfer/item/SlotItemHandler.java
@@ -2,14 +2,6 @@ package slimeknights.mantle.transfer.item;
 
 import javax.annotation.Nonnull;
 
-import com.google.common.collect.Iterators;
-import io.github.fabricators_of_create.porting_lib.transfer.TransferUtil;
-import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
-import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
-import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
-import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage;
-import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
@@ -17,99 +9,85 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
 public class SlotItemHandler extends Slot {
-	private static final Container emptyInventory = new SimpleContainer(0);
-	private final Storage<ItemVariant> itemHandler;
-	private final int index;
+  private static final Container emptyInventory = new SimpleContainer(0);
+  private final IItemHandler itemHandler;
+  private final int index;
 
-	public SlotItemHandler(Storage<ItemVariant> itemHandler, int index, int xPosition, int yPosition) {
-		super(emptyInventory, index, xPosition, yPosition);
-		this.itemHandler = itemHandler;
-		this.index = index;
-	}
+  public SlotItemHandler(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+    super(emptyInventory, index, xPosition, yPosition);
+    this.itemHandler = itemHandler;
+    this.index = index;
+  }
 
-	@Override
-	public boolean mayPlace(@Nonnull ItemStack stack) {
-		return !stack.isEmpty();
-	}
+  @Override
+  public boolean mayPlace(@Nonnull ItemStack stack) {
+    if (stack.isEmpty())
+      return false;
+    return itemHandler.isItemValid(index, stack);
+  }
 
-	@Override
-	@Nonnull
-	public ItemStack getItem() {
-    try (Transaction t = TransferUtil.getTransaction()) {
-      if (Iterators.size(getItemHandler().iterator(t)) <= index) return ItemStack.EMPTY;
-      StorageView<ItemVariant> view = Iterators.get(this.getItemHandler().iterator(t), index);
-      return view.getResource().toStack((int) view.getAmount());
-    }
-	}
+  @Override
+  @Nonnull
+  public ItemStack getItem() {
+    if (getItemHandler().getSlots() <= index) return ItemStack.EMPTY;
+    return this.getItemHandler().getStackInSlot(index);
+  }
 
-	@Override
-	public void set(@Nonnull ItemStack stack) {
-    if (getItemHandler() instanceof InventoryStorage inventoryStorage) {
-      SingleSlotStorage<ItemVariant> slot = inventoryStorage.getSlot(index);
-      TransferUtil.clearStorage(slot);
-      TransferUtil.insertItem(slot, stack);
+  @Override
+  public void set(@Nonnull ItemStack stack) {
+    ((IItemHandlerModifiable) this.getItemHandler()).setStackInSlot(index, stack);
+    this.setChanged();
+  }
+
+  @Override
+  public void onQuickCraft(@Nonnull ItemStack oldStackIn, @Nonnull ItemStack newStackIn) {
+
+  }
+
+  @Override
+  public int getMaxStackSize() {
+    return this.itemHandler.getSlotLimit(this.index);
+  }
+
+  @Override
+  public int getMaxStackSize(@Nonnull ItemStack stack) {
+    ItemStack maxAdd = stack.copy();
+    int maxInput = stack.getMaxStackSize();
+    maxAdd.setCount(maxInput);
+
+    IItemHandler handler = this.getItemHandler();
+    ItemStack currentStack = handler.getStackInSlot(index);
+    if (handler instanceof IItemHandlerModifiable) {
+      IItemHandlerModifiable handlerModifiable = (IItemHandlerModifiable) handler;
+
+      handlerModifiable.setStackInSlot(index, ItemStack.EMPTY);
+
+      ItemStack remainder = handlerModifiable.insertItem(index, maxAdd, true);
+
+      handlerModifiable.setStackInSlot(index, currentStack);
+
+      return maxInput - remainder.getCount();
     } else {
-      try (Transaction t = TransferUtil.getTransaction()) {
-        getItemHandler().insert(ItemVariant.of(stack), stack.getCount(), t);
-        t.commit();
-      }
+      ItemStack remainder = handler.insertItem(index, maxAdd, true);
+
+      int current = currentStack.getCount();
+      int added = maxInput - remainder.getCount();
+      return current + added;
     }
-		this.setChanged();
-	}
+  }
 
-	@Override
-	public void onQuickCraft(@Nonnull ItemStack oldStackIn, @Nonnull ItemStack newStackIn) {
+  @Override
+  public boolean mayPickup(Player playerIn) {
+    return !this.getItemHandler().extractItem(index, 1, true).isEmpty();
+  }
 
-	}
+  @Override
+  @Nonnull
+  public ItemStack remove(int amount) {
+    return this.getItemHandler().extractItem(index, amount, false);
+  }
 
-	@Override
-	public int getMaxStackSize(@Nonnull ItemStack stack) {
-		ItemStack maxAdd = stack.copy();
-		int maxInput = stack.getMaxStackSize();
-		maxAdd.setCount(maxInput);
-    try (Transaction t = TransferUtil.getTransaction()) {
-      Storage<ItemVariant> handler = this.getItemHandler();
-      StorageView<ItemVariant> slot = Iterators.get(handler.iterator(t), index);
-      ItemStack currentStack = slot.getResource().toStack((int) slot.getAmount());
-      if (handler instanceof InventoryStorage inventoryStorage) {
-        SingleSlotStorage<ItemVariant> slotStorage = inventoryStorage.getSlot(index);
-        TransferUtil.clearStorage(slotStorage);
-
-        long remainder = 0;
-        try (Transaction emulate = TransferUtil.getTransaction()) {
-          remainder = TransferUtil.insertItem(inventoryStorage.getSlot(index), maxAdd);
-        }
-        TransferUtil.insertItem(slotStorage, currentStack);
-
-        return (int) (maxInput - remainder);
-      } else {
-        try (Transaction emulate = TransferUtil.getTransaction()) {
-          long remainder = TransferUtil.insertItem(handler, maxAdd);
-
-          int current = currentStack.getCount();
-          long added = maxInput - remainder;
-          return (int) (current + added);
-        }
-      }
-    }
-	}
-
-	@Override
-	public boolean mayPickup(Player playerIn) {
-    if (getItemHandler() instanceof InventoryStorage inventoryStorage)
-      return !TransferUtil.extractAnyItem(inventoryStorage.getSlot(index), 1).isEmpty();
-		return !TransferUtil.extractAnyItem(this.getItemHandler(), 1).isEmpty();
-	}
-
-	@Override
-	@Nonnull
-	public ItemStack remove(int amount) {
-    if (getItemHandler() instanceof InventoryStorage inventoryStorage)
-      return TransferUtil.extractAnyItem(inventoryStorage.getSlot(index), amount);
-		return TransferUtil.extractAnyItem(this.getItemHandler(), amount);
-	}
-
-	public Storage<ItemVariant> getItemHandler() {
-		return itemHandler;
-	}
+  public IItemHandler getItemHandler() {
+    return itemHandler;
+  }
 }
