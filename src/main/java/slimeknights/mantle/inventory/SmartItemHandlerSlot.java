59270f0fc9a1851a8976351611f64be426daddbe
alpha <alphamodeyt@gmail.com>
2022-07-23 19:54:33 -0500

    transfer? I barely know her

--- src/main/java/slimeknights/mantle/inventory/SmartItemHandlerSlot.java
+++ src/main/java/slimeknights/mantle/inventory/SmartItemHandlerSlot.java
@@ -1,18 +1,18 @@
 package slimeknights.mantle.inventory;
 
-import io.github.fabricators_of_create.porting_lib.extensions.SlotExtensions;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.minecraft.world.item.ItemStack;
-import slimeknights.mantle.transfer.item.IItemHandler;
 import slimeknights.mantle.transfer.item.SlotItemHandler;
 
 /** Forge still uses dumb vanilla logic for determining slot limits instead of their own method */
 public class SmartItemHandlerSlot extends SlotItemHandler {
-	public SmartItemHandlerSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+	public SmartItemHandlerSlot(Storage<ItemVariant> itemHandler, int index, int xPosition, int yPosition) {
 		super(itemHandler, index, xPosition, yPosition);
 	}
 
 	@Override
 	public int getMaxStackSize(ItemStack stack) {
-		return getItemHandler().getSlotLimit(((SlotExtensions)this).getSlotIndex());
+		return 64;
 	}
 }
