03f2938e99665dae5dae2aff612c73c063c314d0
alpha <alphamodeyt@gmail.com>
2022-08-02 14:14:30 -0500

    Implement JEI and hopefully fix data gen ingredients

--- src/main/java/slimeknights/mantle/recipe/data/ItemNameIngredient.java
+++ src/main/java/slimeknights/mantle/recipe/data/ItemNameIngredient.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -70,7 +71,7 @@ public class ItemNameIngredient extends AbstractIngredient {
 
     @Override
     public Collection<ItemStack> getItems() {
-      throw new UnsupportedOperationException();
+      return Collections.emptyList();
     }
 
     @Override
