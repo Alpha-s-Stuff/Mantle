d41885a214c9158da5705f892b68b897a725cc96
alpha <alphamodeyt@gmail.com>
2022-08-11 23:46:44 -0500

    use IItemHandlerModifiable

--- src/main/java/slimeknights/mantle/transfer/ItemStorageBlockDataHandler.java
+++ src/main/java/slimeknights/mantle/transfer/ItemStorageBlockDataHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import slimeknights.mantle.transfer.item.IItemHandler;
+import slimeknights.mantle.transfer.item.IItemHandlerModifiable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -57,7 +58,10 @@ public class ItemStorageBlockDataHandler {
 
   public static IItemHandler getCachedHandler(BlockEntity be) {
     ItemStack[] data = CACHED_DATA.get(be.getBlockPos());
-    return new IItemHandler() {
+    return new IItemHandlerModifiable() {
+
+      @Override
+      public void setStackInSlot(int slot, ItemStack stack) {}
 
       @Override
       public int getSlots() {
