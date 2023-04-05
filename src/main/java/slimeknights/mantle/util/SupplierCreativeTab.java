a77e7a5c1b0d58ef573e6297bfe14a9f40369b2e
AlphaMode <alphamodeyt@gmail.com>
2023-04-04 20:55:57 -0500

    Minimal version of SupplierCreativeTab

--- src/main/java/slimeknights/mantle/util/SupplierCreativeTab.java
+++ src/main/java/slimeknights/mantle/util/SupplierCreativeTab.java
@@ -1,37 +1,25 @@
-//package slimeknights.mantle.util;
-//
-//import net.fabricmc.fabric.impl.item.group.ItemGroupExtensions;
-//import net.minecraft.world.item.CreativeModeTab;
-//import net.minecraft.world.item.ItemStack;
-//
-//import java.util.function.Supplier;
-//
-///**
-// * Item group that sets its item based on an item supplier
-// */
-//public class SupplierCreativeTab extends CreativeModeTab {
-//  private final Supplier<ItemStack> supplier;
-//
-//  /**
-//   * Creates a new item group
-//   * @param modId     Tab owner mod ID
-//   * @param name      Tab name
-//   * @param supplier  Item stack supplier
-//   */
-//  public SupplierCreativeTab(String modId, String name, Supplier<ItemStack> supplier) {
-//    super(createTabIndex(), String.format("%s.%s", modId, name));
-//    this.setRecipeFolderName(String.format("%s/%s", modId, name));
-//    this.supplier = supplier;
-//  }
-//
-//  @Override
-//  public ItemStack makeIcon() {
-//    return supplier.get();
-//  }
-//
-//  private static int createTabIndex() {
-//    ((ItemGroupExtensions)CreativeModeTab.TAB_BUILDING_BLOCKS).fabric_expandArray();
-//    return CreativeModeTab.TABS.length - 1;
-//  }
-//
-//}
+package slimeknights.mantle.util;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Supplier;
+
+/**
+ * Item group that sets its item based on an item supplier
+ */
+public class SupplierCreativeTab {
+  /**
+   * Creates a new item group
+   * @param modId     Tab owner mod ID
+   * @param name      Tab name
+   * @param supplier  Item stack supplier
+   */
+  public static CreativeModeTab.Builder create(String modId, String name, Supplier<ItemStack> supplier) {
+    return FabricItemGroup.builder(new ResourceLocation(modId, name)).title(Component.translatable(String.format("%s/%s", modId, name))).icon(supplier);
+  }
+
+}
