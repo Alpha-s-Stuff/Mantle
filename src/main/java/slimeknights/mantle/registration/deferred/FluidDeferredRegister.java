27df6d38cd20d3f430d9a861ec9cbca6a3d6f443
alpha <alphamodeyt@gmail.com>
2023-04-12 17:58:09 -0500

    Instantly register fluids instead of deferring them part 3

--- src/main/java/slimeknights/mantle/registration/deferred/FluidDeferredRegister.java
+++ src/main/java/slimeknights/mantle/registration/deferred/FluidDeferredRegister.java
@@ -166,21 +166,15 @@ public class FluidDeferredRegister extends DeferredRegisterWrapper<Fluid> {
     SimpleDirectionalFluid.Properties props = builder.block(blockObj).buildUpsideDownFluid(stillDelayed, flowingDelayed);
 
     // create fluids now that we have props
-    Supplier<F> stillSup = () -> {
-      F fluid = Registry.register(BuiltInRegistries.FLUID, new ResourceLocation(this.modID, name), still.apply(props));
-      FluidAttributes attributes = props.attributes.build(fluid);
-      FluidVariantAttributes.register(fluid, new FluidAttributeHandler(attributes));
-      EnvExecutor.runWhenOn(EnvType.CLIENT, () -> () -> FluidRenderHandlerRegistry.INSTANCE.register(fluid, new FluidAttributeClientHandler(attributes)));
-      return fluid;
-    };
+    F stillFluid = Registry.register(BuiltInRegistries.FLUID, new ResourceLocation(this.modID, name), still.apply(props));
+    FluidAttributes attributes = props.attributes.build(stillFluid);
+    FluidVariantAttributes.register(stillFluid, new FluidAttributeHandler(attributes));
+    F flowingFluid = Registry.register(BuiltInRegistries.FLUID, new ResourceLocation(this.modID, "flowing_" + name), flowing.apply(props));
+    FluidVariantAttributes.register(flowingFluid, new FluidAttributeHandler(attributes));
+    EnvExecutor.runWhenOn(EnvType.CLIENT, () -> () -> FluidRenderHandlerRegistry.INSTANCE.register(stillFluid, flowingFluid, new FluidAttributeClientHandler(attributes)));
+    Supplier<F> stillSup = () -> stillFluid;
+    Supplier<F> flowingSup = () -> flowingFluid;
     stillDelayed.setSupplier(stillSup);
-    Supplier<F> flowingSup = () -> {
-      F fluid = Registry.register(BuiltInRegistries.FLUID, new ResourceLocation(this.modID, "flowing_" + name), flowing.apply(props));
-      FluidAttributes attributes = props.attributes.build(fluid);
-      FluidVariantAttributes.register(fluid, new FluidAttributeHandler(attributes));
-      EnvExecutor.runWhenOn(EnvType.CLIENT, () -> () -> FluidRenderHandlerRegistry.INSTANCE.register(fluid, new FluidAttributeClientHandler(attributes)));
-      return fluid;
-    };
     flowingDelayed.setSupplier(flowingSup);
 
     // return the final nice object
