c2ca75631912a5acae1048bd32ea38b97b25c4f1
screret <joona.v.stenberg@gmail.com>
2023-05-06 13:46:31 +0300

    made item tags look fabric-like

--- src/main/java/slimeknights/mantle/registration/object/MetalItemObject.java
+++ src/main/java/slimeknights/mantle/registration/object/MetalItemObject.java
@@ -26,10 +26,10 @@ public class MetalItemObject extends ItemObject<Block> {
     super(block);
     this.ingot = ingot;
     this.nugget = nugget;
-    this.blockTag = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("c", "storage_blocks/" + tagName));
-    this.blockItemTag = getTag("storage_blocks/" + tagName);
-    this.ingotTag = getTag("ingots/" + tagName);
-    this.nuggetTag = getTag("nuggets/" + tagName);
+    this.blockTag = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("c", tagName + "_storage_blocks"));
+    this.blockItemTag = getTag(tagName + "_storage_blocks");
+    this.ingotTag = getTag(tagName + "_ingots");
+    this.nuggetTag = getTag(tagName + "_nuggets");
   }
 
   /** Gets the ingot for this object */
