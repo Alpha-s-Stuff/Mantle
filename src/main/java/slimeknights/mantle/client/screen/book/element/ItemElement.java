e725a2a7006091f00106cc25c4c7ca7c8e889bac
alpha <alphamodeyt@gmail.com>
2023-06-03 14:06:03 -0500

    Fix items not rendering in books

--- src/main/java/slimeknights/mantle/client/screen/book/element/ItemElement.java
+++ src/main/java/slimeknights/mantle/client/screen/book/element/ItemElement.java
@@ -99,10 +99,10 @@ public class ItemElement extends SizedBookElement {
       poses.mulPoseMatrix(matrixStack.last().pose());
 
       ItemStack stack = this.itemCycle.get(this.currentItem);
-      this.mc.getItemRenderer().renderAndDecorateItem(matrixStack, stack, 0, 0);
+      this.mc.getItemRenderer().renderAndDecorateItem(new PoseStack(), stack, 0, 0);
       Font font = fontRenderer;//net.minecraftforge.client.RenderProperties.get(stack).getFont(stack);
       if (font == null) font = mc.font;
-      this.mc.getItemRenderer().renderGuiItemDecorations(matrixStack, font, stack, 0, 0, null);
+      this.mc.getItemRenderer().renderGuiItemDecorations(new PoseStack(), font, stack, 0, 0, null);
 
       matrixStack.popPose();
       poses.popPose();
